60ee08c0b43d0d41c2849f1eab3bd7760a7c72d4
iurysantosroque-sys <iurysantosroque@gmail.com>
2026-08-26 23:40:52 -0300

    chore: publicar versão 36.12.1

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE='inova-licitacoes-v36.12.0';
+const CACHE='inova-licitacoes-v36.12.1';
 const CORE=[
-  './','./index.html','./styles.css?v=36.12.0','./app.js?v=36.12.0','./manifest.json',
+  './','./index.html','./styles.css?v=36.12.1','./app.js?v=36.12.1','./manifest.json',
   './assets/logo.png','./icons/icon-192.png','./icons/icon-512.png'
 ];
 
