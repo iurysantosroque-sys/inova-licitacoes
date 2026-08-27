0e3b8e00dd9f08d6846d22714132130ffc496c5c
iurysantosroque-sys <iurysantosroque@gmail.com>
2026-08-27 09:18:16 -0300

    Publica versão 36.12.5

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-const CACHE='inova-licitacoes-v36.12.4';
+const CACHE='inova-licitacoes-v36.12.5';
 const CORE=[
-  './','./index.html','./styles.css?v=36.12.4','./app.js?v=36.12.4','./manifest.json?v=36.12.4',
-  './assets/logo.png','./icons/icon-192-v2.png?v=36.12.4','./icons/icon-512-v2.png?v=36.12.4'
+  './','./index.html','./styles.css?v=36.12.5','./app.js?v=36.12.5','./manifest.json?v=36.12.5',
+  './assets/logo.png','./icons/icon-192-v2.png?v=36.12.5','./icons/icon-512-v2.png?v=36.12.5'
 ];
 
 self.addEventListener('install',event=>{
