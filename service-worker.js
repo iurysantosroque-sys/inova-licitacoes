accbbc466f8e480ce6c3f3387902d5d49c7ff16a
iurysantosroque-sys <iurysantosroque@gmail.com>
2026-08-27 00:20:40 -0300

    Publica versão 36.12.3

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE='inova-licitacoes-v36.12.2';
+const CACHE='inova-licitacoes-v36.12.3';
 const CORE=[
-  './','./index.html','./styles.css?v=36.12.2','./app.js?v=36.12.2','./manifest.json',
+  './','./index.html','./styles.css?v=36.12.3','./app.js?v=36.12.3','./manifest.json',
   './assets/logo.png','./icons/icon-192.png','./icons/icon-512.png'
 ];
 
